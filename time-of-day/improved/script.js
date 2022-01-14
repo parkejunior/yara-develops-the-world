46d332ef08a7a0d963a6bfaaf965b7e423068724
Patrick Luan <you@your-domain.com>
2022-01-14 17:50:01 -0300

    Improve time-of-day in script.js

--- time-of-day/improved/script.js
+++ time-of-day/improved/script.js
@@ -1,29 +1,79 @@
-function carregar() {
+const timer = (function () {
+  function changeTime(date) {
+    const currentDate = date instanceof Date ? date : new Date()
+    let hour = currentDate.getHours()
+    let minute = currentDate.getMinutes()
+    let second = currentDate.getSeconds()
 
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('image')
-var data = new Date()
-//var hora = data.getHours()
-//var minutes = data.getMinutes()
-var hora = 23
-var minutes = 10
-msg.innerHTML = `<strong> Now is ${hora}:${minutes} </strong>`
-if(hora >= 0 && hora < 12) {
-    //Good morning
-    img.src = 'goodmorning.png'
-    document.body.style.background = '#fcd67e'
+    hour = twoDigits(hour)
+    minute = twoDigits(minute)
+    second = twoDigits(second)
     
-}else if (hora >= 12 && hora < 18) {
-    //Good afternoon
-    img.src='goodafternoon.png'
-    document.body.style.background = '#e9926a'
-    document.getElementById("title").style.color = "#fcf8e6"
-} else {
-    //Good evening
-    img.src='goodnight2.png'
-    document.body.style.background = '#046961'
-    document.getElementById("title").style.color = "#f4edcd";
-}
+    const background = document.body
+    const title = document.getElementById("title")
+    const msgTarget = document.getElementById('message')
+    const imgTarget = document.getElementById('image')
 
-}
+    const times = {
+      morning: {
+        image: 'goodmorning.png',
+        background: '#fcd67e',
+        title: '',
+      },
+      afternoon: {
+        image: 'goodafternoon.png',
+        background: '#e9926a',
+        title: "#fcf8e6",
+      },
+      evening: {
+        image: 'goodnight2.png',
+        background: '#046961',
+        title: "#f4edcd",
+      },
+    }
 
+    let currentTime = {}
+
+    if (hour >= 0 && hour < 12) {
+      currentTime = times.morning
+    } else if (hour >= 12 && hour < 18) {
+      currentTime = times.afternoon
+    } else {
+      currentTime = times.evening
+    }
+
+    const message = document.createElement('strong')
+    message.innerHTML = `Now is ${hour}:${minute}:${second}`
+
+    const image = document.createElement('img')
+    image.src = `img/${currentTime.image}`
+    image.alt = "Photo of the day"
+
+    title.style.color = currentTime.title
+    background.style.background = currentTime.background
+    background.style.transition = 'linear 1s'
+
+    msgTarget.innerHTML = ''
+    msgTarget.appendChild(message)
+
+    imgTarget.innerHTML = ''
+    imgTarget.appendChild(image)
+  }
+
+  function twoDigits(number) {
+    return number < 10 ? '0' + number : number;  
+  }
+
+  function autoUpdateTime() {  
+    const interval = 1000
+    
+    changeTime();
+
+    setTimeout(autoUpdateTime, interval);
+  }
+
+  return {
+    changeTime,
+    autoUpdateTime,
+  }
+})()
